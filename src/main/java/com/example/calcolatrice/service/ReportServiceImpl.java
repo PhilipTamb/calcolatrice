f91af853dc59303853a60f70cb79c252b613e803
Di Zenzo Carmine Federico <114579198+dizenzoc@users.noreply.github.com>
2024-12-16 10:06:16 +0100

    update ReportServiceImpl
    aggiunta import

--- src/main/java/com/example/calcolatrice/service/ReportServiceImpl.java
+++ src/main/java/com/example/calcolatrice/service/ReportServiceImpl.java
@@ -5,6 +5,7 @@ import com.example.calcolatrice.model.Report;
 import com.example.calcolatrice.repository.ReportRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import com.example.calcolatrice.exception.NoDataFoundException;
 
 import java.util.List;
 import java.util.Optional;
